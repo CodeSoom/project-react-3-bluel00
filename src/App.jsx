d7b34d892d035f0b996caf2bfb6bc6e21c6c6f07
EL <ickyicky@gmail.com>
2021-04-05 20:29:58 +0900

    배포 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,87 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <header>
-        <h1>
-          RESCAT
-        </h1>
-      </header>
+      <title>RESCAT</title>
+
+      <ul
+        style={{
+          display: 'flex',
+
+          overflow: 'auto',
+
+          flexWrap: 'wrap',
+        }}
+      >
+        <li>
+          <div>
+            <figure>
+              <span>
+                <img
+                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
+                  style={{ height: '200px' }}
+                  alt=""
+                />
+              </span>
+            </figure>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <figure>
+              <span>
+                <img
+                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
+                  style={{ height: '200px' }}
+                  alt=""
+                />
+              </span>
+            </figure>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <figure>
+              <span>
+                <img
+                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
+                  style={{ height: '200px' }}
+                  alt=""
+                />
+              </span>
+            </figure>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <figure>
+              <span>
+                <img
+                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
+                  style={{ height: '200px' }}
+                  alt=""
+                />
+              </span>
+            </figure>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <figure>
+              <span>
+                <img
+                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
+                  style={{ height: '200px' }}
+                  alt=""
+                />
+              </span>
+            </figure>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
