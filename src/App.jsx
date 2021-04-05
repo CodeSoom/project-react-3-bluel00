8ef3a2a66b13df0e6ed446a67470e970ebefe7c1
EL <ickyicky@gmail.com>
2021-04-05 21:18:34 +0900

    충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -3,84 +3,11 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <title>RESCAT</title>
-      <ul
-        style={{
-          display: 'flex',
-          overflow: 'auto',
-          flexWrap: 'wrap',
-        }}
-      >
-        <li>
-          <div>
-            <figure>
-              <span>
-                <img
-                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
-                  style={{ height: '200px' }}
-                  alt=""
-                />
-              </span>
-            </figure>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <figure>
-              <span>
-                <img
-                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
-                  style={{ height: '200px' }}
-                  alt=""
-                />
-              </span>
-            </figure>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <figure>
-              <span>
-                <img
-                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
-                  style={{ height: '200px' }}
-                  alt=""
-                />
-              </span>
-            </figure>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <figure>
-              <span>
-                <img
-                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
-                  style={{ height: '200px' }}
-                  alt=""
-                />
-              </span>
-            </figure>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <figure>
-              <span>
-                <img
-                  src="https://thingd-media-ec.thefancy.com/default/1170923538786292415_a6fa88e16da1.jpg"
-                  style={{ height: '200px' }}
-                  alt=""
-                />
-              </span>
-            </figure>
-          </div>
-        </li>
-      </ul>
+      <header>
+        <h1>
+          RESCAT!
+        </h1>
+      </header>
     </div>
   );
 }
